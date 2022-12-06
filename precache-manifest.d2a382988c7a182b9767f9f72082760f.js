eea1cc6994b485e761ff9a57dd54cc110eb512e5
ShantanuSutar <92261216+ShantanuSutar@users.noreply.github.com>
2022-12-06 23:28:26 +0530

    Updates

---
+++ precache-manifest.d2a382988c7a182b9767f9f72082760f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c9a5f99274baca12523",
+    "url": "/Lights_Out/static/css/main.6a8fdcf0.chunk.css"
+  },
+  {
+    "revision": "1c9a5f99274baca12523",
+    "url": "/Lights_Out/static/js/main.1c9a5f99.chunk.js"
+  },
+  {
+    "revision": "9b314b03da5604077d84",
+    "url": "/Lights_Out/static/js/1.9b314b03.chunk.js"
+  },
+  {
+    "revision": "200ed0e57224e51ab18b",
+    "url": "/Lights_Out/static/js/runtime~main.200ed0e5.js"
+  },
+  {
+    "revision": "10c7e26290739aff0727d4ba4ce3b6cd",
+    "url": "/Lights_Out/index.html"
+  }
+];
